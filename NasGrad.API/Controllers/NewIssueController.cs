171f51cb2735865d7a1edd9d0a3b22c335b3e93f
Dalibor Kovacevic <d.kovacevic@levi9.com>
2018-12-07 23:26:31 +0100

    new item added not in db

--- NasGrad.API/Controllers/NewIssueController.cs
+++ NasGrad.API/Controllers/NewIssueController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NasGrad.DBEngine;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -13,72 +14,26 @@ namespace NasGrad.API.Controllers
     {
         private IDBStorage _dbStorage;
 
-        [HttpPost]
-        public async Task<IActionResult> NewIssue([FromBody] JObject newIssue)
+        public NewIssueController(IDBStorage dBStorage)
         {
-            var issue = new NasGradIssue();
+            _dbStorage = dBStorage;
+        }
+
+        [HttpPost]
+        public ActionResult NewIssue([FromBody] JObject newIssue)
+        {
+            var issue = JsonConvert.DeserializeObject<NasGradIssue>(newIssue.GetValue("issue").ToString());
+
+            var pictureName = newIssue.GetValue("pictureInfo").Value<string>("name");
+            var pictureB64 = newIssue.GetValue("pictureInfo").Value<string>("rawData");
+
             return Ok("Issue added");
         }
     }
-
-    public class IssueLocation
-    {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
-
-
-    public class NasGradIssue
-    {
-        public string Id { get; set; }
-        public string OwnerId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string IssueType { get; set; }
-        public StateEnum State { get; set; }
-        public List<string> Pictures { get; set; }
-        public List<string> Categories { get; set; }
-        public IssueLocation Location { get; set; }
-        public byte[] PicturePreview { get; set; }
-    }
-
-    public enum StateEnum
-    {
-        Submitted = 1,
-        Reported = 2,
-        Done = 3
-    }
-
 }
-
 /*
-Issue
-{
-    "id" : GUID,
-    "owner-id": GUID, //citizen guid
-    
-    "title": string (100),
-    "description": string,
-
-    "issue-type": string, //predefined enum
-    "categories": [string], //enum category
-    "pictures": [picture-id-mdb], //id of picture in mongo db
-    "state": string //enum of states
-
-    "location": {
-        "langitude": double,
-        "longitude": double
-    }
-}
-
-/api/getIssueList
-{
-    "count": int,
-    "issues": [
-        {
-            "issue": Issue,
-            "picture-preview": byte[]
-        }
-    ]
-}
-*/
+var dbCollection = _database.GetCollection<NasGradPicture>(Constants.PictureTableName);
+var pic = await dbCollection.Find(c => string.Equals(c.Id, id)).ToListAsync();
+pic[0].Visible = visible;
+                await dbCollection.InsertOneAsync(pic[0]);
+                return true;*/
