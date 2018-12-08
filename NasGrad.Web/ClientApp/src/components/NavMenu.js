b65c2ab0dec71b4bd7285e2492c103a8f6919e23
Darko Stevanovic <da.stevanovic@levi9.com>
2018-12-08 05:43:36 +0100

    WIP

--- NasGrad.Web/ClientApp/src/components/NavMenu.js
+++ NasGrad.Web/ClientApp/src/components/NavMenu.js
@@ -1,5 +1,4 @@
 ﻿import React from 'react';
-import { Link } from 'react-router-dom';
 import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
@@ -8,7 +7,7 @@ const NavMenu = props => (
     <Navbar inverse fixedTop fluid collapseOnSelect>
         <Navbar.Header>
             <Navbar.Brand>
-                <Link to={'/'}>Naš Grad</Link>
+                <a href={'/'}>Naš Grad</a>
             </Navbar.Brand>
             <Navbar.Toggle />
         </Navbar.Header>
