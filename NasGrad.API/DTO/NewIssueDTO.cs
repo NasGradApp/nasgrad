14151d087413d5822d66de0b7654089f100d666d
Dalibor Kovacevic <d.kovacevic@levi9.com>
2019-05-19 15:47:16 +0200

    v2 for adding issue

---
+++ NasGrad.API/DTO/NewIssueDTO.cs
@@ -0,0 +1,18 @@
+﻿using NasGrad.DBEngine;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NasGrad.API.DTO
+{
+    public class NewIssueDTO
+    {
+        [JsonProperty("issue")]
+        public NasGradIssue Issue { get; set; }
+
+        [JsonProperty("pictureInfo")]
+        public NasGradPicture PictureInfo { get; set; }
+    }
+}
